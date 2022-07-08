6e4c3b4d1cb450f9e94604aaa30f52e1535cd0f9
Joshua Hendricks (work) <hendricksjc@gmail.com>
2022-07-08 12:04:11 -0600

    update scripts

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -6,10 +6,12 @@ fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.dotfiles/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
+    echo "language"
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
+    echo "command" 
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
 
---
+++ bin/.local/bin/tmux-snippets.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Convert this to RUST or GO
+
+selected=$(find ~/.dotfiles/snippets -type f | tr ' ' '\n' | fzf)
+
+#cat $selected | pbcopy ;
+
+
+# Might be nice to identify any queries it has and take user input to fill them out, then copy. 
+# Maybe based on brackets like {class name} then "Enter class name: " prompt.  Replace all {class name} with query.
+file_content=$(cat $selected)
+
+#queries=($(echo "$file_content" | grep -Eo '{{.*}}'))
+
+queries=$(grep -Eo '{{.*}}' <<< "$file_content")
+
+echo "Queries found $queries"
+
+#while IFS= read -r line; do
+#  echo "lines $line"
+#  read -p "Enter $line: " query
+#  file_content=${file_content//$i/$query}
+#done <<< "$queries"
+
+#while IFS= read -r line; do
+#  read -u 1 -p "Query $line: " 
+#  file_content=${file_content//$line/$REPLY}
+#done <<< $queries
+
+echo $file_content
+
+for i in $queries
+ do
+    read -p "Enter $i: " query
+    file_content=${file_content//$i/$query}
+    echo $file_content
+ done
+
+echo $file_content | pbcopy ;
